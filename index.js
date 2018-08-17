641e4b011a3b578255d41f63b05c273c0518f9f8
shrijan <shrijan00003@gmail.com>
2018-08-17 11:28:49 +0545

    from tutsplus

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const port = 3700;
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+
 app.get("/", function(req, res){
     res.render("page");
 });
 
-// app.get("/", function(req, res){
-//     res.send("It works!");
-// });
+app.use(express.static(__dirname + '/public'));
 
-app.listen(port);
-console.log("Listening on port " + port);
+var io = require('socket.io').listen(app.listen(port));
+
+console.log("Listening on port " + io);
 
